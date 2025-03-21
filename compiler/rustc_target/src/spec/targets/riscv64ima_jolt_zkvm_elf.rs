a4d2ba083d06c6de3abb892ecccf04698e92f9c1
Maksim Ryndin <maksim.ryndin@gmail.com>
2025-03-21 16:18:39 +0000

    rv64ima

--- compiler/rustc_target/src/spec/targets/riscv64ima_jolt_zkvm_elf.rs
+++ compiler/rustc_target/src/spec/targets/riscv64ima_jolt_zkvm_elf.rs
@@ -3,9 +3,9 @@ use crate::spec::{Target, TargetOptions};
 
 pub fn target() -> Target {
     Target {
-        data_layout: "e-m:e-p:32:32-i64:64-n32-S128".into(),
-        llvm_target: "riscv32".into(),
-        pointer_width: 32,
+        data_layout: "e-m:e-p:64:64-i64:64-n32:64-S128".into(),
+        llvm_target: "riscv64".into(),
+        pointer_width: 64,
         arch: "riscv32".into(),
 
         metadata: crate::spec::TargetMetadata {
@@ -20,8 +20,9 @@ pub fn target() -> Target {
             vendor: "jolt".into(),
             linker_flavor: LinkerFlavor::Gnu(Cc::No, Lld::Yes),
             linker: Some("rust-lld".into()),
-            cpu: "generic-rv32".into(),
-            features: "+m".into(),
+            cpu: "generic-rv64".into(),
+            llvm_abiname: "lp64".into(),
+            features: "+m,+a".into(),
             max_atomic_width: Some(64),
             atomic_cas: true,
             executables: true,
